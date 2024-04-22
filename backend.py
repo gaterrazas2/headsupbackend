c78680e78b26d2f3f75841965774222af37fe719
Gabriel Terrazas <Gaboon@Gabes-Mac.lan>
2024-04-22 14:34:23 -0600

    inital commit

---
+++ backend.py
@@ -0,0 +1,32 @@
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson import json_util
+
+class Backend:
+    def __init__(self) -> None:
+        # database setup
+        self.uri = "mongodb+srv://gterra06:Gt391299%21%21@cluster0.o458oeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        self.client = MongoClient(self.uri, server_api=ServerApi('1'))
+        self.db = self.client['LittleBrotherBlog']
+        self.collection = self.db['Posts']
+
+    def sendToDB(self, formData):
+
+        # inserting form data in db 
+        self.collection.insert_one(formData)
+        print("Hey I posted your data fam")
+        return  
+
+    async def getFromDB(self, category):
+        if category in {'comics', 'music', 'games', 'sports', 'events'}:
+            query = {'category': category}
+            documents = list(self.collection.find(query))
+            # Convert ObjectId to string for serialization
+            serialized_documents = [json_util.dumps(doc) for doc in documents]
+            return serialized_documents
+        else:
+            return []
+
+
+
---
+++ routes.py
@@ -0,0 +1,43 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from backend import Backend
+
+app = Flask(__name__)
+CORS(app) 
+backend = Backend()
+
+# Send to DB
+@app.route("/post", methods=['POST'])
+def print_request():
+    if request.method == 'POST':
+        data = request.get_data()
+        data_dict = json.loads(data)
+        backend.sendToDB(data_dict)
+    return "got it!" , 200 
+
+# Get from DB
+@app.route("/getComics")
+async def get_comics():
+    result = await backend.getFromDB('comics')
+    return jsonify(result)
+
+@app.route("/getSports")
+async def get_sports():
+    result = await backend.getFromDB('sports')
+    return jsonify(result)
+
+@app.route("/getMusic")
+async def get_music():
+    result = await backend.getFromDB('music')
+    return jsonify(result)
+
+@app.route("/getEvents")
+async def get_events():
+    result = await backend.getFromDB('events')
+    return jsonify(result)
+
+@app.route("/getGames")
+async def get_games():
+    result = await backend.getFromDB('games')
+    return jsonify(result)
