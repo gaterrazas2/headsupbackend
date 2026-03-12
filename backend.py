45e112b3b8f4b6c1dcd41fab8919a4503d9c2cab
Gabriel Terrazas <gterra06@icloud.com>
2026-03-12 17:42:59 -0600

    ls

--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ class Backend:
         self.collection = self.db['Posts']
         self.context = self.load_context()
         self.name = "Gabriel Terrazas"
-        self.openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        self.openai = OpenAI()
 
     def sendToDB(self, formData):
         # inserting form data in db 
